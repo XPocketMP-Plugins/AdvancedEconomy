f5364dbca69401376b7c4649164d7e480ce7c188
XPocketMP-Plugins <fylocode@gmail.com>
2025-01-30 19:10:26 +0700

    Create SellListener.php

---
+++ src/AdvancedEconomy/Listeners/SellListener.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AdvancedEconomy\Listeners;
+
+use pocketmine\event\Listener;
+use AdvancedEconomy\Event\SellEvent;
+use pocketmine\utils\TextFormat;
+
+class SellListener implements Listener {
+
+    public function onSell(SellEvent $event): void {
+        $player = $event->getPlayer();
+        $item = $event->getItem();
+        $amount = $event->getAmount();
+        $price = $event->getPrice();
+
+        $player->sendMessage(TextFormat::GREEN . "You sold $amount " . $item->getName() . " for $price Aether Coins.");
+    }
+}
